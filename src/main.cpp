c84a1d24dc57efeb0ed1f47600f3ba74e31ba737
alex <alexstephens38@gmail.com>
2024-02-10 17:23:09 -0500

    added main.cpp for TCP server comms with python commander

--- src/main.cpp
+++ src/main.cpp
@@ -1,285 +1,106 @@
 #include <Arduino.h>
-#include <ESPmDNS.h>
-#include <esp_task_wdt.h>
+#include <WiFi.h>
+#include <WifiUDP.h>
+#include <WiFiServer.h>
 
-#include "configurable.h"
-#include "gantry.h"
 #include "motorgo_mini.h"
-#include "web_server.h"
 
-TaskHandle_t loop_motorgo_task;
+MotorGo::MotorGoMini motorgo_mini;
+MotorGo::MotorChannel& motor_right = motorgo_mini.ch1;
+MotorGo::MotorParameters motor_params_right;
+MotorGo::PIDParameters velocity_pid_params_right;
 
-Gantry::Gantry gantry("gantry");
+// wifiStuff
+const char* ssid = "NotARobot";
+const char* password = "M1crowave!";
+const char* testReplyPacket = "MotorGoHello";
 
-// Motor parameters
-MotorGo::MotorParameters motor_params_ch0;
-MotorGo::MotorParameters motor_params_ch1;
+// WiFiUDP udp;
+// unsigned int localUdpPort = 4210;
+unsigned int port = 4210;
+WiFiServer server(4210);
+WiFiClient client;
 
-// PID parameters
-MotorGo::PIDParameters position_pid_params_ch0;
-MotorGo::PIDParameters position_pid_params_ch1;
-MotorGo::PIDParameters velocity_pid_params_ch0;
-MotorGo::PIDParameters velocity_pid_params_ch1;
-
-void on_pid_update(const float& updated_value);
-
-// Setup configurables
-ESPWifiConfig::Configurable<float> position_pid_p_ch0(
-    position_pid_params_ch0.p, "/ch0/position/p",
-    "P Gain, Position, Channel 0");
-ESPWifiConfig::Configurable<float> position_pid_i_ch0(
-    position_pid_params_ch0.i, "/ch0/position/i",
-    "I Gain, Position, Channel 0");
-ESPWifiConfig::Configurable<float> position_pid_d_ch0(
-    position_pid_params_ch0.d, "/ch0/position/d",
-    "D Gain, Position, Channel 0");
-ESPWifiConfig::Configurable<float> position_pid_lpf_ch0(
-    position_pid_params_ch0.lpf_time_constant, "/ch0/position/lpf",
-    "LPF, Position, Channel 0");
-
-ESPWifiConfig::Configurable<float> position_pid_p_ch1(
-    position_pid_params_ch1.p, "/ch1/position/p",
-    "P Gain, Position, Channel 1");
-
-ESPWifiConfig::Configurable<float> position_pid_i_ch1(
-    position_pid_params_ch1.i, "/ch1/position/i",
-    "I Gain, Position, Channel 1");
-
-ESPWifiConfig::Configurable<float> position_pid_d_ch1(
-    position_pid_params_ch1.d, "/ch1/position/d",
-    "D Gain, Position, Channel 1");
-
-ESPWifiConfig::Configurable<float> position_pid_lpf_ch1(
-    position_pid_params_ch1.lpf_time_constant, "/ch1/position/lpf",
-    "LPF, Position, Channel 1");
-
-ESPWifiConfig::Configurable<float> velocity_pid_p_ch0(
-    velocity_pid_params_ch0.p, "/ch0/velocity/p",
-    "P Gain, Velocity, Channel 0");
-
-ESPWifiConfig::Configurable<float> velocity_pid_i_ch0(
-    velocity_pid_params_ch0.i, "/ch0/velocity/i",
-    "I Gain, Velocity, Channel 0");
-
-ESPWifiConfig::Configurable<float> velocity_pid_d_ch0(
-    velocity_pid_params_ch0.d, "/ch0/velocity/d",
-    "D Gain, Velocity, Channel 0");
-
-ESPWifiConfig::Configurable<float> velocity_pid_lpf_ch0(
-    velocity_pid_params_ch0.lpf_time_constant, "/ch0/velocity/lpf",
-    "LPF, Velocity, Channel 0");
-
-ESPWifiConfig::Configurable<float> velocity_pid_p_ch1(
-    velocity_pid_params_ch1.p, "/ch1/velocity/p",
-    "P Gain, Velocity, Channel 1");
-
-ESPWifiConfig::Configurable<float> velocity_pid_i_ch1(
-    velocity_pid_params_ch1.i, "/ch1/velocity/i",
-    "I Gain, Velocity, Channel 1");
-
-ESPWifiConfig::Configurable<float> velocity_pid_d_ch1(
-    velocity_pid_params_ch1.d, "/ch1/velocity/d",
-    "D Gain, Velocity, Channel 1");
-
-ESPWifiConfig::Configurable<float> velocity_pid_lpf_ch1(
-    velocity_pid_params_ch1.lpf_time_constant, "/ch1/velocity/lpf",
-    "LPF, Velocity, Channel 1");
-
-void freq_println(float freq, const char* message)
-{
-  static unsigned long last_print_time = 0;
-  unsigned long now = millis();
-
-  if (now - last_print_time > 1000.0 / freq)
-  {
-    Serial.println(message);
-    last_print_time = now;
-  }
-}
-
-void loop_motorgo(void* pvParameters);
+char incomingPacket[255];
+char replyPacket[] = "acknowledged";
 
 void setup()
 {
+  delay(2000);
   Serial.begin(115200);
+  delay(3000);
 
-  delay(1000);
-
-  //   while (digitalRead(0) == HIGH)
-  //   {
-  //     delay(100);
-  //   }
-
-  //   Setup configurable callbacks
-
-  // Setup motor parameters
-  motor_params_ch0.pole_pairs = 11;
-  motor_params_ch0.power_supply_voltage = 5.0;
-  motor_params_ch0.voltage_limit = 5.0;
-  motor_params_ch0.current_limit = 320;
-  motor_params_ch0.velocity_limit = 100.0;
-  motor_params_ch0.calibration_voltage = 1.0;
-
-  motor_params_ch1.pole_pairs = 11;
-  motor_params_ch1.power_supply_voltage = 5.0;
-  motor_params_ch1.voltage_limit = 5.0;
-  motor_params_ch1.current_limit = 320;
-  motor_params_ch1.velocity_limit = 100.0;
-  motor_params_ch1.calibration_voltage = 1.0;
-
-  // Set PID parameters
-  position_pid_params_ch0.p = 30.0;
-  position_pid_params_ch0.i = 0.0;
-  position_pid_params_ch0.d = 0.0;
-  position_pid_params_ch0.output_ramp = 10000.0;
-  position_pid_params_ch0.lpf_time_constant = 0.05;
-  position_pid_params_ch0.limit = 100.0;
-
-  velocity_pid_params_ch0.p = 3.5;
-  velocity_pid_params_ch0.i = 0.0;
-  velocity_pid_params_ch0.d = 0.0;
-  velocity_pid_params_ch0.output_ramp = 10000.0;
-  velocity_pid_params_ch0.lpf_time_constant = 0.05;
-  velocity_pid_params_ch0.limit = 100.0;
-
-  position_pid_params_ch1.p = 30.0;
-  position_pid_params_ch1.i = 0.0;
-  position_pid_params_ch1.d = 0.0;
-  position_pid_params_ch1.output_ramp = 10000.0;
-  position_pid_params_ch1.lpf_time_constant = 0.05;
-  position_pid_params_ch1.limit = 100.0;
-
-  velocity_pid_params_ch1.p = 3.0;
-  velocity_pid_params_ch1.i = 0.0;
-  velocity_pid_params_ch1.d = 0.008;
-  velocity_pid_params_ch1.output_ramp = 10000.0;
-  velocity_pid_params_ch1.limit = 100.0;
-  velocity_pid_params_ch1.lpf_time_constant = 0.05;
-
-  // Setup motor parameters
-  //   motor_params_ch0.pole_pairs = 11;
-  //   motor_params_ch0.power_supply_voltage = 5.0;
-  //   motor_params_ch0.voltage_limit = 5.0;
-  //   motor_params_ch0.current_limit = 320;
-  //   motor_params_ch0.velocity_limit = 100.0;
-  //   motor_params_ch0.calibration_voltage = 3.0;
-
-  //   motor_params_ch1.pole_pairs = 11;
-  //   motor_params_ch1.power_supply_voltage = 5.0;
-  //   motor_params_ch1.voltage_limit = 5.0;
-  //   motor_params_ch1.current_limit = 320;
-  //   motor_params_ch1.velocity_limit = 100.0;
-  //   motor_params_ch1.calibration_voltage = 3.0;
-
-  // Set PID parameters
-  //   position_pid_params_ch0.p = 5.0;
-  //   position_pid_params_ch0.i = 0.0;
-  //   position_pid_params_ch0.d = 0.0;
-  //   position_pid_params_ch0.output_ramp = 10000.0;
-  //   position_pid_params_ch0.lpf_time_constant = 0.1;
-  //   position_pid_params_ch0.limit = 100.0;
-
-  //   velocity_pid_params_ch0.p = 15.0;
-  //   velocity_pid_params_ch0.i = 1.2;
-  //   velocity_pid_params_ch0.d = 0.00;
-  //   velocity_pid_params_ch0.output_ramp = 10000.0;
-  //   velocity_pid_params_ch0.limit = 100.0;
-  //   velocity_pid_params_ch0.lpf_time_constant = 0.11;
-
-  //   position_pid_params_ch1.p = 4.0;
-  //   position_pid_params_ch1.i = 0.0;
-  //   position_pid_params_ch1.d = 0.00;
-  //   position_pid_params_ch1.output_ramp = 10000.0;
-  //   position_pid_params_ch1.lpf_time_constant = 0.10;
-  //   position_pid_params_ch1.limit = 100.0;
-
-  //   velocity_pid_params_ch1.p = 15.0;
-  //   velocity_pid_params_ch1.i = 0.8;
-  //   velocity_pid_params_ch1.d = 0.0;
-  //   velocity_pid_params_ch1.output_ramp = 10000.0;
-  //   velocity_pid_params_ch1.limit = 100.0;
-  //   velocity_pid_params_ch1.lpf_time_constant = 0.12;
-
-  // Setup gantry
-  //   gantry = new Gantry::Gantry("gantry");
-  gantry.setup(motor_params_ch0, motor_params_ch1, position_pid_params_ch0,
-               position_pid_params_ch1, velocity_pid_params_ch0,
-               velocity_pid_params_ch1);
-
-  delay(1000);
-
-  position_pid_p_ch0.set_post_callback(on_pid_update);
-  position_pid_i_ch0.set_post_callback(on_pid_update);
-  position_pid_d_ch0.set_post_callback(on_pid_update);
-  position_pid_lpf_ch0.set_post_callback(on_pid_update);
-
-  position_pid_p_ch1.set_post_callback(on_pid_update);
-  position_pid_i_ch1.set_post_callback(on_pid_update);
-  position_pid_d_ch1.set_post_callback(on_pid_update);
-  position_pid_lpf_ch1.set_post_callback(on_pid_update);
-
-  velocity_pid_p_ch0.set_post_callback(on_pid_update);
-  velocity_pid_i_ch0.set_post_callback(on_pid_update);
-  velocity_pid_d_ch0.set_post_callback(on_pid_update);
-  velocity_pid_lpf_ch0.set_post_callback(on_pid_update);
-
-  velocity_pid_p_ch1.set_post_callback(on_pid_update);
-  velocity_pid_i_ch1.set_post_callback(on_pid_update);
-  velocity_pid_d_ch1.set_post_callback(on_pid_update);
-  velocity_pid_lpf_ch1.set_post_callback(on_pid_update);
-
-  // Start server
-  ESPWifiConfig::WebServer::getInstance().start();
-
-  if (!MDNS.begin("gantry"))
-  {  // Start the mDNS responder for esp32.local  MDNS.addService("http", "tcp",
-     // 8080);
-
-    Serial.println("Error setting up MDNS responder!");
-    while (1)
-    {
-      delay(1000);
-    }
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
   }
-  Serial.println("mDNS responder started");
-  MDNS.addService("http", "tcp", 8080);
-  MDNS.addServiceTxt("http", "tcp", "gantry", "sentry0");
+  server.begin();
+  Serial.println("wifi connected (udp)");
+  Serial.print("IP addy: ");
+  Serial.println(WiFi.localIP());
+  Serial.printf("UDP server on port %d\n", port);
 
-  xTaskCreatePinnedToCore(
-      loop_motorgo,       /* Task function. */
-      "Loop MotorGo",     /* name of task. */
-      10000,              /* Stack size of task */
-      NULL,               /* parameter of the task */
-      1,                  /* priority of the task */
-      &loop_motorgo_task, /* Task handle to keep track of created task */
-      1);                 /* pin task to core 1 */
+  motor_params_right.pole_pairs = 11;
+  motor_params_right.power_supply_voltage = 5.0;
+  motor_params_right.voltage_limit = 5.0;
+  motor_params_right.current_limit = 300;
+  motor_params_right.velocity_limit = 100.0;
+  motor_params_right.calibration_voltage = 2.0;
+  motor_params_right.reversed = true;
+
+  // Setup Ch0
+  bool calibrate = false;
+  motor_right.init(motor_params_right, calibrate);
+
+  // Set velocity controller parameters
+  // Setup PID parameters
+
+  velocity_pid_params_right.p = 1.6;
+  velocity_pid_params_right.i = 0.01;
+  velocity_pid_params_right.d = 0.0;
+  velocity_pid_params_right.output_ramp = 10000.0;
+  velocity_pid_params_right.lpf_time_constant = 0.11;
+
+  motor_right.set_velocity_controller(velocity_pid_params_right);
+
+  //   Set closed-loop velocity mode
+  motor_right.set_control_mode(MotorGo::ControlMode::Voltage);
+
+  //   Enable motors
+  motor_right.enable();
 }
 
-void loop_motorgo(void* pvParameters)
-{
-  Serial.print("Loop FOC running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for (;;)
-  {
-    gantry.loop();
-
-    //   Print loop frequency
-    esp_task_wdt_reset();
+void loop(){
+  // Check if a client has connected
+  if (!client.connected()) {
+    client = server.available();
   }
-}
+  
+  if (client) {
+    if (client.available()) {
+        String message = client.readStringUntil('\n');
+    // Serial.printf("Received packet: %s\n", incomingPacket);
 
-int i = 0;
-// Constrain loop speed to 250 Hz
-unsigned long last_loop_time = 0;
-float vel = 30;
-void loop()
-{
-  // Do nothing
-}
+    // TODO: Parse the incomingPacket to extract motor power levels and act accordingly
+    // cast packet content to double
+    float command = (atof(message.c_str()) - 50.0f)/40.0f;
+    if (command < (-2.50)) command = -2.50;
+    if (command > (2.5)) command = 2.50;
 
-void on_pid_update(const float& updated_value)
-{
-  gantry.update_pid_params(position_pid_params_ch0, position_pid_params_ch1,
-                           velocity_pid_params_ch0, velocity_pid_params_ch1);
+    // Sending a reply (for example, position and velocity data)
+    float position = motor_right.get_position();  // Placeholder for actual position data
+    float velocity = motor_right.get_velocity();  // Placeholder for actual velocity data
+    String reply = String(position) + "," + String(velocity); // Creating a reply string with position and velocity
+
+    client.print(reply);
+    // udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    // udp.write((const uint8_t*)reply.c_str(), reply.length()); // Corrected: Now actually sending the reply
+    // udp.endPacket();
+
+    motor_right.set_target_voltage(command);
+  }
+
+  motor_right.loop();
+}
 }
