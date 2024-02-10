f8d1fcac769a50757fd3b4bf1a3ffa6085afed5b
b-freed <bfreed@andrew.cmu.edu>
2024-02-10 17:09:53 -0500

    start code for environment object for robot

---
+++ python_commander/environment.py
@@ -0,0 +1,19 @@
+
+
+class RobotEnv():
+    def __init__(self,ep_len):
+
+        
+
+    def reset():
+
+    def step(a):
+        power_level = a
+        send_motor_command(power_level)
+        pos_vel_array = receive_reply()
+
+        # process state
+
+        # compute reward
+        r = 
+        return s, r, d, None         
